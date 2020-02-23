5f507810fa0de14b8e8508ded2f9166131a96706
IvelinIskrenov <59844738+IvelinIskrenov@users.noreply.github.com>
2020-02-23 20:06:10 +0200

    Add files via upload

--- Baza/app.js
+++ Baza/app.js
@@ -12,10 +12,13 @@ function preload()
 {
     game.load.tilemap('Mario' , 'libs/MyTileSet.json', null , Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles' , 'libs\\32x32_tileset_mario.png');
+    game.load.image('player' , 'libs\\player.png');
 }
 var map;
 var sloifiziki;
 var kartinki;
+var player;
+var cursor;
 function create() 
 {
     map = game.add.tilemap('Mario');
@@ -25,14 +28,78 @@ function create()
     sloifiziki.scale.setTo(1);
     kartinki.scale.setTo(1);
     sloifiziki.resizeWorld();
-    map.setCollision([1008,404,403],true);
-    
+    map.setCollision([1008,404,403], true , sloifiziki);
 
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    player = game.add.sprite(10,640,'player');
+    player.scale.setTo(0.32);
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    player.body.linearDaping = 1;
+    cursor = game.input.keyboard.createCursorKeys();
+    game.camera.follow(player); 
+    
+    player.body.gravity.y += 30;
+    // on floor
     console.log("Created");
 }
-
+var right = false;
+var left = false;
+var down = false;
+var up = false;
+var isOntheFloor = 0;
 function update() 
 {
+    right = false;
+    left = false;
+    down = false;
+    up = false;
+    if(cursor.right.isDown)
+    {
+        right = true;
+        player.body.x += 2;
+    }
+    if(cursor.left.isDown)
+    {
+        left = true;
+        player.body.x -= 2;
+    }
+    if(cursor.down.isDown)
+    {
+        down = true
+        player.body.y += 2;
+    }
+    if (cursor.up.isDown && player.body.wasTouching.down)
+    {
+        player.body.velocity.y = -350;
 
+    }
+    // if(cursor.up.isDown)
+    // {
+    //     up = true
+    //     player.body.y -= 2;
+    // }
+
+
+    game.physics.arcade.collide(player,sloifiziki, () =>{
+        // if(right)
+        // {
+        //     player.body.x -= 2;
+        // }
+        // if(left)
+        // {
+        //     player.body.x += 2;
+        // }
+        // if(down)
+        // {
+        //     player.body.y -= 2;
+        // }
+        // if(up)
+        // {
+        //     player.body.y += 2;
+        // }
+        
+    } );
 }
 // alt shift r
